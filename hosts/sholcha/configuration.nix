069b9fc98f85e322dfab99bb34be345a7c679551
wolf4am <wolf4am@gmail.com>
2026-03-25 14:57:00 +0500

    Changed bootloader

--- hosts/sholcha/configuration.nix
+++ hosts/sholcha/configuration.nix
@@ -13,8 +13,9 @@
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
 
