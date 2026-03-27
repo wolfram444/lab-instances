646dda53bed4e55d08859bd871dfdd28746719c2
wolf4am <wolf4am@gmail.com>
2026-03-27 17:18:35 +0500

    Nginx port fixed issue

--- hosts/sholcha/configuration.nix
+++ hosts/sholcha/configuration.nix
@@ -147,18 +147,19 @@
   # nginx
   services.nginx = {
     enable = true;
-    listen = [
-      {
-        addr = "0.0.0.0";
-        port = 8080;
-      }
-    ];
+
     virtualHosts.localhost = {
       locations."/" = {
         return = "200 '<html><body>It works</body></html>'";
         extraConfig = ''
           default_type text/html;
         '';
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 8080;
+          }
+        ];
       };
     };
   };
