5e6a0676ad9a317f25c87a6bf2d8ea2d32714365
wolf4am <wolf4am@gmail.com>
2026-03-27 16:54:28 +0500

    add nginx

--- hosts/sholcha/configuration.nix
+++ hosts/sholcha/configuration.nix
@@ -144,6 +144,19 @@
     };
   };
 
+  # nginx
+  services.nginx = {
+    enable = true;
+    virtualHosts.localhost = {
+      locations."/" = {
+        return = "200 '<html><body>It works</body></html>'";
+        extraConfig = ''
+          default_type text/html;
+        '';
+      };
+    };
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
