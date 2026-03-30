0e7e2c4a9d69f78b0975973d9b327679b77ed6b3
wolf4am <wolf4am@gmail.com>
2026-03-30 19:54:40 +0500

    minecraft

--- hosts/sholcha/configuration.nix
+++ hosts/sholcha/configuration.nix
@@ -41,6 +41,30 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
+  # for minecraft server
+  nixpkgs.config.allowUnfree = true;
+
+  services.minecraft-server = {
+    enable = true;
+    eula = true;
+    openFirewall = true; # Opens the port the server is running on (by default 25565 but in this case 43000)
+    declarative = true;
+    whitelist = {
+      # This is a mapping of Minecraft usernames to to the players' UUIDs
+      username1 = "3e220001-9544-48bb-8fd0-ca7139727207";
+    };
+    serverProperties = {
+      server-port = 25565;
+      difficulty = 3;
+      gamemode = 1;
+      max-players = 5;
+      motd = "NixOS Minecraft server!";
+      white-list = true;
+      allow-cheats = true;
+    };
+    jvmOpts = "-Xms2048M -Xmx2048M";
+  };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
