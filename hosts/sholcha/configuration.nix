37caa32baa683e5d98ae84bc3d222645cfa9cd37
wolf4am <wolf4am@gmail.com>
2026-03-31 12:44:47 +0500

    test

--- hosts/sholcha/configuration.nix
+++ hosts/sholcha/configuration.nix
@@ -9,7 +9,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     inputs.relago-support.nixosModules.server
-    ../../modules
+    ../../modules/services/minecraft
   ];
 
   services.relago-server.enable = true;
