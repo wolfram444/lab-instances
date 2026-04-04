6e386794c028d27bc36942051eef12274e11d222
wolf4am <wolf4am@gmail.com>
2026-04-04 18:36:02 +0500

    chore: change port againclear

--- modules/nixos/garage/default.nix
+++ modules/nixos/garage/default.nix
@@ -12,7 +12,7 @@
       data_dir = "/var/lib/garage/data";
       metadata_dir = "/var/lib/garage/meta";
 
-      rpc_bind_addr = "0.0.0.0:443";
+      rpc_bind_addr = "0.0.0.0:3901";
       rpc_secret = "b8ed42b061bee4500b4fbe783ef87b2be78a8e58fdb6318278c9ee492c408c27";
 
       s3_api = {
